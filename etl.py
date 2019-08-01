0e9262512449bb2a322ed428f5c6657fbf3ead7c
Kenji Chiu <kenji.cchiu@gmail.com>
2019-08-01 14:37:18 +0800

    assert df index unique

--- etl.py
+++ etl.py
@@ -7,6 +7,7 @@ logger = logging.getLogger(__name__ + ' module')
 
 
 class Compare:
+
     def __init__(self, new_ex_json: List[Dict],
                  old_ex_json: Optional[List[Dict]],
                  pairs: Optional[Dict] = None) -> None:
@@ -19,11 +20,11 @@ class Compare:
         self.to_delete = []
 
         if old_ex_json is None:
-            df = Data_process(new_ex_json, exhibitor=True, pairs=pairs).df
+            df = Data_process(new_ex_json,  pairs=pairs).df
             self.to_create = df
         else:
-            self.old_ex_df = Data_process(old_ex_json, exhibitor=True, pairs=pairs).df
-            self.new_ex_df = Data_process(new_ex_json, exhibitor=True, pairs=pairs, latest=True).df
+            self.old_ex_df = Data_process(old_ex_json,  pairs=pairs).df
+            self.new_ex_df = Data_process(new_ex_json,  pairs=pairs, latest=True).df
             self.map_objectID_for_comparsion()
             self.find_delta_records()
 
@@ -81,7 +82,6 @@ class Compare:
 class Data_process:
 
     def __init__(self, json_data: List[dict],
-                 exhibitor: bool = False,
                  pairs: Dict = None,
                  latest: bool = False) -> None:
 
@@ -89,29 +89,23 @@ class Data_process:
 
         if latest:
             self.drop_df_cols()  # Drop irrelevant cols (e.g. password, contact person)
-        if exhibitor:
-            self.indexing_exhibitor_df()  # Set 'ref_no' as index
-            if pairs:
-                self.rename_df_custom_field_column(pairs)  # Map columns name for comparison
-        if latest and exhibitor and pairs:
+        if self.df.index.name != 'ref_no':  # Set 'ref_no' as index if not already
+            self.indexing_exhibitor_df()
+        if pairs:
+            self.rename_df_custom_field_column(pairs)  # Map columns name for comparison
+        if latest and pairs:
             self.normalize_country_field()
 
-
-    # @staticmethod
-    # def map_objectID_for_comparsion(old_df: pd.DataFrame, new_df: pd.DataFrame) -> pd.DataFrame:
-    #     old_df = old_df[old_df.index.isin(new_df.index)].sort_index()
-    #     cols = old_df.columns.tolist()
-    #     # DataFrame index & columns order has to be identical for comparison
-    #     # Map objectID to new_df
-    #     new_df_with_oid =\
-    #         new_df[new_df.index.isin(old_df.index)].merge(old_df['objectID'], on='ref_no').sort_index()[cols]
-    #     return new_df_with_oid
+        assert self.df.index.nunique() == self.df.shape[0]  # Asserting index is unique
 
     def normalize_country_field(self) -> 'Data_process':
         """ Removing trail ; in country field
         :return: self
         """
-        self.df['Country'] = self.df['Country'].apply(lambda x: x.replace(';', ''))
+        try:
+            self.df['Country'] = self.df['Country'].apply(lambda x: x.replace(';', ''))
+        except KeyError as e:
+            print('Column {} not availabel in DF'.format(e))
         return self
 
     def indexing_exhibitor_df(self) -> "Data_process":
@@ -136,35 +130,3 @@ class Data_process:
         self.df[col] = self.df[col].apply(func)
         return self
 
-
-
-
-# class Df:
-#     """ Contains a handful of dataFrame method
-#     : param df: dataFrame
-#     """
-#
-#     def __init__(self, json_data: List[dict]) -> None:
-#         self.df = pd.DataFrame(json_data)
-#
-#     def indexing_exhibitor_df(self) -> "Df":
-#         self.df = self.df.set_index('ref_no')
-#         return self
-#
-#     def rename_df_custom_field_column(self, pairs: Dict[str, str]) -> "Df":
-#         self.df = self.df.rename(pairs, axis='columns')
-#         return self
-#
-#     def drop_df_cols(self, cols_to_drop: Optional[List[str]] = None) -> 'Df':
-#         if cols_to_drop is None:
-#             cols_to_drop = ['contact_no', 'email', 'full_name', 'position']  # Avoid mutable default args
-#         try:
-#             self.df = self.df.drop(cols_to_drop, axis=1)
-#         except KeyError as e:
-#             logger.warning(e)
-#             pass
-#         return self
-#
-#     def modify_col(self, col: Union[str, List[str]], func: Callable[[Any], Any]) -> "Df":
-#         self.df[col] = self.df[col].apply(func)
-#         return self
